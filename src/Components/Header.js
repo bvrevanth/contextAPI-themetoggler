8f1303efce83e653dc788f175a48b8bb6ad4a93d
bvrevanth <revanth47.2017@gmail.com>
2023-08-17 13:02:00 +0530

    pushing code

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import ThemeToggler from "./ThemeToggler";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Theme Toggler</h1>
+      <ThemeToggler />
+    </header>
+  );
+};
+
+export default Header;
